5e3129f64800ebd4e8ec7a12c3f447f623546227
Hao Tran <haotranpunchman@gmail.com>
2022-06-21 11:10:07 +0700

    update routes

--- superRoutes/routesUser.ts
+++
@@ -1,9 +0,0 @@
-import { UserController } from "../controllers/user";
-import { Router } from "./Router";
-import http from "http"
-const router = new Router()
-const controllers = new UserController()
-
-// router.get("/user/create-admin",)
-
-export default router
